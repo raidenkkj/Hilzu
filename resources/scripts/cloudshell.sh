0f12939dd3277119053a0f46075aaa04c4eed360
Raiden Ishigami <contact.raidenishi69@gmail.com>
2023-04-10 14:43:23 -0300

    create cloudshell.sh

---
+++ resources/scripts/cloudshell.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+bash <(curl -s https://raw.githubusercontent.com/raidenkkj/Hilzu/master/resources/scripts/ubuntu_install.sh)
+
+npm install -g pm2
+
+current_dir=$(pwd)
+if [ "$current_dir" != "$HOME/Loader" ]; then
+  cd "$HOME/Loader"
+fi
+
+pm2 start run --interpreter bash
+pm2 startup
+pm2 save
